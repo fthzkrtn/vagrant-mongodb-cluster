c3659547b73ce98e721762de6acb9578b0b33ccd
Fatih Karatana <fatih.karatana@tai.com.tr>
2020-11-02 15:56:22 +0300

    Setup.sh fixed

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/sh
 mongo --host 192.168.22.10 << 'EOF'
-config = { _id: "mongo-replica-set", members:[
+config = { _id: "mrs", members:[
           { _id : 0, host : "192.168.22.10:27017"},
           { _id : 1, host : "192.168.22.20:27017"},
-          { _id : 2, host : "192.168.22.30:27017"} ]
+          { _id : 2, host : "192.168.22.30:27017"},
           { _id : 3, host : "192.168.22.40:27017"} ]
          };
 rs.initiate(config);
